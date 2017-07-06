96a4a90b631cfb6e5ab44520de0d500a56ead9f0
bkhk06 <bkhk06@users.noreply.github.com>
2017-07-06 09:59:13 +0800

    Update bayes.py

--- bayes.py
+++ bayes.py
@@ -82,32 +82,31 @@ def bagOfWords2VecMN(vocabList,inputSet):
 def textPasrse(bigString):
     import re
     listOfTokens  = re.split(r'\W*',bigString)
-    print("\n------------**************----------------\n")
-    print([tok.lower() for tok in listOfTokens if len(tok) >2])
     return [tok.lower() for tok in listOfTokens if len(tok) >2]
 
 def spamTest():
     docList = [];classList = [];fullText = []
     for i in range(1,26):
         fr_spam = open('email/spam/%d.txt' % i,encoding="ISO-8859-1").read()
-        #print(fr_spam)
+        print(fr_spam)
         wordList = textPasrse(fr_spam)
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(1)
         fr_ham = open('email/ham/%d.txt' % i,encoding="ISO-8859-1").read()
-        #print(fr_ham)
+        print(fr_ham)
         wordList = textPasrse(fr_ham)
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(0)
         vocabList = createVocabList(docList)
-    trainingSet = list(range(50));testSet=[]
+    trainingSet = list(range(50));testSet=[]#need to convert range type into list, otherwise, will report TypeError: 
+                                           # 'range' object doesn't support item deletion for del
     for i in range(10):
         randIndex = int(random.uniform(0,len(trainingSet)))
         testSet.append(trainingSet[randIndex])
         del(trainingSet[randIndex])
-    trainMat = [];trainingClasses = []
+        trainMat = [];trainingClasses = []
     for docIndex in trainingSet:
         trainMat.append(setOfWords2Vec(vocabList,docList[docIndex]))
         trainingClasses.append(classList[docIndex])
@@ -120,8 +119,52 @@ def spamTest():
             print("classification error", docList[docIndex])
     print ('the error rate is: ', float(errorCount) / len(testSet))
     # return vocabList,fullText
+
+def calcMostFreq(vocabList,fullText):
+    import operator
+    freqDict = {}
+    for token in vocabList:
+        freqDict[token] = fullText.count(token)
+    sortedFreq = sorted(freqDict.items(),key=operator.itemgetter(1),
+                        reverse=True)
+    return sortedFreq[:30]
+
+def localWords(feed1,feed0):
+    import feedparser
+    docList = [];classList = [];fullText = []
+    minLen = min(len(feed1['entries']),len(feed0['entries']))
+    for i in range(minLen):
+        wordList = textPasrse(feed1['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)
+    top30Words = calcMostFreq(vocabList,fullText)
+    for pairW in top30Words:
+        if pairW[0] in vocabList: vocabList.remove(pairW[0])
+    trainingSet = list(range(2*minLen));testSet = []
+    for i in range(20):
+        randIndex = int(random.uniform(0,len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat=[];trainClasses=[]
+    for docIndex in trainingSet:
+        trainingSet.append(bagOfWords2VecMN(vocabList,docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = bagOfWords2VecMN(vocabList,docList[docIndex])
+        if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+            errorCount +=1
+        errorCount += 1
+    print('the error rate is: ',float(errorCount)/len(testSet))
+    return vocabList,p0V,p1V
+
+
 if __name__ == "__main__":
     import bayes
+    import feedparser
 
     listOPosts, listClasses = bayes.loadDataSet()
     print(listOPosts)
@@ -138,7 +181,6 @@ if __name__ == "__main__":
     trainMat = []
     for postinDoc in listOPosts:
         trainMat.append(bayes.setOfWords2Vec(myVocabList,postinDoc))
-    import os
 
     p0V,p1V,pAb = bayes.trainNB0(trainMat,listClasses)
 
@@ -147,3 +189,7 @@ if __name__ == "__main__":
     bayes.testingNB()
 
     bayes.spamTest()
+
+    #ny=feedparser.parse('http://newyork.craigslist.org/stp/index.rss')
+    #sf=feedparser.parse('http://sfbay.craigslist.org/stp/index.rss')
+    #vocalList,pSF,pNY = bayes.localWords(ny,sf)
