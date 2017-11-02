ef6ece4e4aabdddc4325ea3bcea06eaf5a578030
bkhk06 <bkhk06@users.noreply.github.com>
2017-11-02 14:50:05 +0800

    Update fp_Growth.py

--- fp_Growth.py
+++ fp_Growth.py
@@ -20,3 +20,77 @@ class treeNode:
         for child in self.children.values():
             child.disp(ind+1)
 
+def createTree(dataSet,minSup=1):
+    headerTable = {}
+    for trans in dataSet:
+        for item in trans:
+            headerTable[item] = headerTable.get(item,0) + dataSet[trans]
+    for k in list(headerTable):#for k in headerTable.keys():
+        if headerTable[k] < minSup:
+            del (headerTable[k])
+    freqItemSet = set(headerTable.keys())
+    if len(freqItemSet) ==0:return None,None
+    for k in headerTable:
+        headerTable[k] = [headerTable[k],None]
+    retTree = treeNode('Null Set',1,None)
+    for tranSet,count in dataSet.items():
+        localID = {}
+        for item in tranSet:
+            if item in freqItemSet:
+                localID[item] = headerTable[item][0]
+        if len(localID)>0:
+            orderedItems = [v[0] for v in sorted(localID.items(),key=lambda p:p[1],reverse=True)]
+            updateTree(orderedItems,retTree,headerTable,count)
+    return retTree,headerTable
+
+def updateTree(items,inTree,headerTable,count):
+    if items[0] in inTree.children:
+        inTree.children[items[0]].inc(count)
+    else:
+        inTree.children[items[0]]=treeNode(items[0],count,inTree)
+        if headerTable[items[0]][1] == None:
+            headerTable[items[0]][1]=inTree.children[items[0]]
+        else:
+            updateHeader(headerTable[items[0]][1],inTree.children[items[0]])
+    if len(items)>1:
+        updateTree(items[1::],inTree.children[items[0]],headerTable,count)
+
+def updateHeader(nodeToTest,targerNode):
+    while(nodeToTest.nodeLink!=None):
+        nodeToTest=nodeToTest.nodeLink
+    nodeToTest.nodeLink=targerNode
+
+def loadSimpDat():
+    simpDat = [['r', 'z', 'h', 'j', 'p'],
+               ['z', 'y', 'x', 'w', 'v', 'u', 't', 's'],
+               ['z'],
+               ['r', 'x', 'n', 'o', 's'],
+               ['y', 'r', 'x', 'z', 'q', 't', 'p'],
+               ['y', 'z', 'x', 'e', 'q', 's', 't', 'm']]
+    return simpDat
+
+def createInitSet(dataSet):
+    retDict = {}
+    for trans in dataSet:
+        retDict[frozenset(trans)] = 1
+    return retDict
+
+
+if __name__ == "__main__":
+    import fp_Growth
+    print("##########Create Tree:############\n")
+    rootNode = treeNode('pyramid',9,None)
+    rootNode.children['eye']=treeNode('eye',13,None)
+    rootNode.disp()
+    rootNode.children['phoenix']=fp_Growth.treeNode('phoenix',3,None)
+    print("\n########## Tree:############\n")
+    rootNode.disp()
+
+    print("\n########## fpGrowth Tree:############\n")
+    simpDat = loadSimpDat()
+    print("simpDat:\n",simpDat)
+    initSet = createInitSet(simpDat)
+    print("initSet:\n",initSet)
+    myFPtree,myHeaderTab = createTree(initSet,3)
+    myFPtree.disp()
+
