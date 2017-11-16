ef1da8b966e95e927ebb90b29f8f01ecafb75fc3
bkhk06 <bkhk06@users.noreply.github.com>
2017-11-16 09:01:18 +0800

    Create new file

---
+++ pca.py
@@ -0,0 +1,20 @@
+from numpy import *
+
+def loadDataSet(fileName,delim='\t'):
+    fr = open(fileName)
+    stringArr = [line.split(delim) for line in fr.readlines()]
+    datArr = [map(float,line ) for line in stringArr]
+    return mat(datArr)
+
+def pca(dataMat,topNfeat = 9999999):
+    meanVals = mean(dataMat,axis=0)
+    meanRemoved = dataMat - meanVals
+    covMat = cov(meanRemoved, rowvar=0)
+    eigVals, eigVects = linalg.eig(covMat)
+    eigInd = argsort(eigVals)
+    eigInd = eigInd[:-(topNfeat+1):-1]
+    redEigVects = eigVects[:,eigInd]
+    lowDDataMat = meanRemoved*redEigVects
+    reconMat = (lowDDataMat*redEigVects.T) + meanVals
+    return lowDDataMat,reconMat
+
